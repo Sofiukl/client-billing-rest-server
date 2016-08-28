14420dc1d9b61acaaf26120285ac4c737dea6e47
Rajatech <sofikul23feb1990@gmail.com>
2016-08-28 23:06:49 +0530

    Modified main class

--- client-billing-core/src/main/java/com/sofikultech/nextgen/Application.java
+++ client-billing-core/src/main/java/com/sofikultech/nextgen/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages="com.sofikultech.nextgen.core")
+@ComponentScan(basePackages="com.sofikultech.nextgen")
 @EnableAutoConfiguration
 public class Application {
 
